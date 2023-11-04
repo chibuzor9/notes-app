dc99b1b1aa234e2cb64c0b92af71f4b5808c2209
Chibuzor Emmanuel <emmanuelchibuzor2005@gmail.com>
2023-11-04 16:27:45 +0100

    modified:   package.json 	modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,31 @@
-import { Component } from '@angular/core'
-import { HeaderComponent } from './header/header.component'
-import { NotesListComponent } from './notes-list/notes-list.component'
-import { NgFor } from '@angular/common'
-import { FormsModule } from '@angular/forms'
-import { ReactiveFormsModule } from '@angular/forms'
-import { FormGroup, FormControl } from '@angular/forms'
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { NotesListComponent } from './notes-list/notes-list.component';
+import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  imports: [
-    HeaderComponent,
-    NgFor,
-    NotesListComponent,
-    FormsModule,
-    ReactiveFormsModule,
-  ],
+	selector: 'app-root',
+	standalone: true,
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
+	imports: [
+		HeaderComponent,
+		NgFor,
+		NotesListComponent,
+		FormsModule,
+		ReactiveFormsModule,
+	],
 })
-
 export class AppComponent {
-  login = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
-  })
-  name = new FormControl('')
-  showName() {
-    alert(this.name.value)
-  }
+	login = new FormGroup({
+		username: new FormControl(''),
+		password: new FormControl(''),
+	});
+	name = new FormControl('');
+	showName() {
+		alert(this.name.value);
+	}
 }
