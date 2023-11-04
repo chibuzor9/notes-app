45062db75ef233025533d59a08c1690aef916d28
Chibuzor Emmanuel <emmanuelchibuzor2005@gmail.com>
2023-11-04 07:57:19 +0100

    modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   imports: [HeaderComponent]
